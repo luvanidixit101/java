8dc999794516b7355ccb0174fd9ca0a60b4b2c1b
luvanidixit101 <luvanidixit101@gmail.com>
2025-07-31 03:12:42 +0000

    q3

---
+++ q3.java
@@ -0,0 +1,3 @@
+/*write a program  that accept a String and a charecter as input. 
+The program shold print the frequency in the string and list all the positin (indices) 
+where the charecter appears*/
